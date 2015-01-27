11b963ff940565c2143baf3a029e283935c0ed8e
Adam <ovadia.adam@gmail.com>
2015-01-26 20:43:53 -0500

    Update validus.mk

--- validus.mk
+++ validus.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/hltespr/full_hltespr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/valius/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)

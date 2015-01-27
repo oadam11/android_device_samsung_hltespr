c06005c3b0fb580562fbf0d776c99fe1c57662ab
Adam <ovadia.adam@gmail.com>
2015-01-26 20:29:40 -0500

    Update and rename cm.mk to validus.mk

---
+++ validus.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/hltespr/full_hltespr.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/valius/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hltespr
+PRODUCT_NAME := validus_hltespr
